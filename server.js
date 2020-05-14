5d24e96569abfbcc95462c94a254707d20e5b419
Nimzyow <n_soufiani@hotmail.com>
2020-05-14 17:31:35 +0100

    created user.js, added middleware to parse body and updated README

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const app = express();
 
+// middleware
+app.use(express.json({ extended: false }));
+
 // Define Routes
 app.use("/auth", require("./routes/auth"));
+app.use("/users", require("./routes/users"));
 
 module.exports = app;
