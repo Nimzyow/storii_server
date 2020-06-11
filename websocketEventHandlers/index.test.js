79c8c66117dbb093c4096d3bf10dffe5780fbfb2
Laura Jackson <playeronereadystart@gmail.com>
2020-06-11 17:20:11 +0100

    include sinon and test index of WebSocketEventHandlers

---
+++ websocketEventHandlers/index.test.js
@@ -0,0 +1,43 @@
+const chai = require("chai");
+const sinon = require("sinon");
+const sinonChai = require("sinon-chai");
+const WebSocketEventHandlers = require("./index");
+const entryHandler = require("./Entry/entryHandler");
+
+chai.use(sinonChai);
+const { expect } = chai;
+
+describe.only("WebSocketEventHandlers", () => {
+  describe("message", () => {
+    const message = { content: "hi", writer: {} };
+    let events;
+    let emitter;
+    let entryHandlerPostStub;
+    let emitStub;
+
+    beforeEach(() => {
+      emitter = {
+        emit: () => {},
+      };
+      emitStub = sinon.stub(emitter, "emit");
+      events = WebSocketEventHandlers(emitter);
+      entryHandlerPostStub = sinon.stub(entryHandler, "post").resolves();
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it("calls it's entryHandler.post with message", async () => {
+      await events.message(message);
+
+      expect(entryHandlerPostStub).to.have.been.calledWith(message);
+    });
+
+    it("emits the new-message after successful entry post", async () => {
+      await events.message(message);
+
+      expect(emitStub).to.have.been.calledWith("new-message", message);
+    });
+  });
+});
